16f7498f0f8ea0d32cd903470bc8a653210d8c3d
hhakangull <hakanngul@icloud.com>
2023-06-11 21:35:52 +0100

    Initial Commit

---
+++ Swift-Udemy-Education/Swift_Udemy_EducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Udemy_EducationApp.swift
+//  Swift-Udemy-Education
+//
+//  Created by Hakan Gül on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Udemy_EducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

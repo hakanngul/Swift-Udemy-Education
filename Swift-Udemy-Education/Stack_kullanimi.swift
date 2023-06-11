f38bfaa4a3969eba0b4a814d30c34b59246b7f5b
Hakan Gul <hakanngul@icloud.com>
2023-06-11 23:16:03 +0100

    Frame struct kullanımı

--- Swift-Udemy-Education/Stack_kullanimi.swift
+++ Swift-Udemy-Education/Stack_kullanimi.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct Stack_kullanimi: View {
     var body: some View {
         HStack{
-            Text("Merhaba").frame(maxHeight:.infinity).background(.yellow)
-            Rectangle().fill(.red).frame(width: 100, height: 100)
-            Rectangle().fill(.green).frame(width: 50, height: 50)
-            Rectangle().fill(.blue).frame(width: 30, height: 30)
+            VStack{
+                KirmiziKare().frame(width: 100,height: 100)
+                YesilKare()
+                Rectangle().fill(.blue).frame(width: 50, height: 50)
+            }
+            VStack{
+                Yazi(icerik: "Merhaba", yaziBoyutu: 30)
+            }
         }
     }
 }
@@ -21,5 +25,26 @@ struct Stack_kullanimi: View {
 struct Stack_kullanimi_Previews: PreviewProvider {
     static var previews: some View {
         Stack_kullanimi()
+    }
+}
+
+struct Yazi: View {
+    var icerik = ""
+    var yaziBoyutu = 0
+    var body: some View {
+        Text(icerik).font(.system(size: CGFloat(yaziBoyutu)))
+    }
+}
+
+struct KirmiziKare: View {
+    var body: some View {
+        Rectangle().fill(.red)
+    }
+}
+
+
+struct YesilKare: View {
+    var body: some View {
+        Rectangle().fill(.green).frame(width: 80, height: 80)
     }
 }

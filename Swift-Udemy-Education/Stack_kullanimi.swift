d993b8427ca0db971769d8acc6dadbf5d4dae4c1
Hakan Gul <hakanngul@icloud.com>
2023-06-11 23:08:09 +0100

    bottom ve top farklı padding vermek

--- Swift-Udemy-Education/Stack_kullanimi.swift
+++ Swift-Udemy-Education/Stack_kullanimi.swift
@@ -11,7 +11,7 @@ struct Stack_kullanimi: View {
     var body: some View {
         HStack{
             Rectangle().fill(.red).frame(width: 100, height: 100)
-                .padding([.leading, .trailing],30)
+                .padding(.top, 30).padding(.bottom, 50)
             Rectangle().fill(.green).frame(width: 50, height: 50)
             Rectangle().fill(.blue).frame(width: 30, height: 30)
         }
